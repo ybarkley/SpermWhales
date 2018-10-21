a74e758a66a13252f4930e1fca2f34356263f5fd
ybarkley <ybarkley@hawaii.edu>
2018-10-21 09:43:01 -1000

    How to enable the RStudio Push/Pull buttons

---
+++ HowToPushPull.r
@@ -0,0 +1,9 @@
+# Make the git repo first in my github profile. 
+## Then create new repo in Rstudio but ''Clone'' Repo and use the https from github. 
+
+#to make the Push and Pull buttons work in Rstudio, make test script or copy a script into the directory of the project on the computer. Now there is a file to commit at push to the cloned repo. use the following commands
+
+git commit -m "initial commit"
+git push origin master
+
+#This should push the scrip to the git repo and enable the push pull buttons. If not, try restarting RStudio.
