b49fc3439e5e48f68803ba1b5806002f6216699f
ybarkley <ybarkley@hawaii.edu>
2020-04-07 08:35:38 -1000

    Cleaned up 2_PmEnvData Finished Effort code Included some plotting of data on maps

--- code/ExploreData.r
+++ code/ExploreData.r
@@ -8,26 +8,34 @@ library(reshape2)
 
 #load data set
 dir = 'C:\\Users\\yvers\\Documents\\CHP 3\\SpermWhales\\output/'
-SwEnvData_tot <- read.csv(paste0(dir, 'SpermiesWithEnvData_20200310.csv'))
+SwEnvData_tot <- read.csv(paste0(dir, 'SpermiesWithEnvData_20200326.csv'))
 
 
-# center data with 'colMeans()', from https://www.gastonsanchez.com/visually-enforced/how-to/2014/01/15/Center-data-in-R/
 
-center_colmeans <- function(x) {
-  xcenter = colMeans(x)
-  x - rep(xcenter, rep.int(nrow(x), ncol(x)))
-}
+# center and scale predictors
+SwEnvData_pred <- select(SwEnvData_tot, lat, lon2, sstAQ_m:wavepow)
+SwEnvData_scale <- scale(SwEnvData_pred, scale = TRUE)
+SwEnvData_scale <- cbind(select(SwEnvData_tot, ID:type, pdist), SwEnvData_scale)
+write.csv(SwEnvData_scale, here::here('output', 'SpermiesWithEnvData_20200326-scaled.csv'), row.names = F)
 
-# apply it
-SwEnvData_pred <- select(SwEnvData_tot, lat, lon2, sstAQ_m:wind_ms)
 
-SwEnvData_cent <- center_colmeans(SwEnvData_pred)
-SwEnvData_cent <- cbind(select(SwEnvData_tot, ID:type, pdist), SwEnvData_cent)
+
+# OR center data with 'colMeans()', from https://www.gastonsanchez.com/visually-enforced/how-to/2014/01/15/Center-data-in-R/
+
+#mean-centering function
+# center_colmeans <- function(x) {
+#   xcenter = colMeans(x)
+#   x - rep(xcenter, rep.int(nrow(x), ncol(x)))
+# }
+
+# SwEnvData_cent <- center_colmeans(SwEnvData_pred)
+# SwEnvData_cent <- cbind(select(SwEnvData_tot, ID:type, pdist), SwEnvData_cent)
 
 
 
 #melt data into long format then plot
-SwSome <- select(SwEnvData_cent, ID, sstAQ_m:wind_ms)
+SwSome <- select(SwEnvData_scale, ID, sstAQ_m:wavepow)
+
 SwDynamic <- select(SwSome, -(c(bath, d2land_km, slp_deg, asp_deg)))
 # SwDynamic <- select(SwSome, ID, chla_m)
 SwMeltDyn <- melt(SwDynamic, id.vars = 'ID')
@@ -35,32 +43,32 @@ SwMeltDyn <- melt(SwDynamic, id.vars = 'ID')
 boxDyn <- ggplot(SwMeltDyn, aes(variable, value)) +
   geom_boxplot()
 boxDyn
-ggsave(paste0(dir, 'boxplot_dynamic.png'), width = 6, height = 4)
+ggsave(paste0(dir, 'boxplot_dynamic-scaled.png'), width = 6, height = 4)
 # ggsave(paste0(dir, 'boxplot_chla.png'), width = 6, height = 4)
 # ggsave(paste0(dir, 'boxplot_ssh.png'), width = 6, height = 4)
 
 
 ###
-SwStatic <- select(SwEnvData_cent, ID, bath, d2land_km, slp_deg, asp_deg)
-# SwStatic <- select(SwEnvData_cent, ID, slp_deg)
+SwStatic <- select(SwEnvData_scale, ID, bath, d2land_km, slp_deg, asp_deg)
+SwStatic <- select(SwEnvData_tot, ID, d2land_km)
 SwMeltStat <- melt(SwStatic, id.vars = 'ID')
 
 boxStat <- ggplot(SwMeltStat, aes(variable, value)) +
   geom_boxplot()
 boxStat
 
-ggsave(paste0(dir, 'boxplot_static.png'), width = 6, height = 4)
+ggsave(paste0(dir, 'boxplot_static-scaled.png'), width = 6, height = 4)
 # ggsave(paste0(dir, 'boxplot_slope.png'), width = 6, height = 4)
 
 
 ###
-SwLatLon <- select(SwEnvData_cent, ID, lat, lon2)
+SwLatLon <- select(SwEnvData_scale, ID, lat, lon2)
 SwMeltLatLon <- melt(SwLatLon, id.vars = 'ID')
 
 boxlatLon <- ggplot(SwMeltLatLon, aes(variable, value)) +
   geom_boxplot()
 boxlatLon
-ggsave(paste0(dir, 'boxplot_latlon.png'), width = 6, height = 4)
+ggsave(paste0(dir, 'boxplot_latlon-scaled.png'), width = 6, height = 4)
 
 
 
